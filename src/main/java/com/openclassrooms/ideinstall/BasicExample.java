21956d829ba196b7b560f8852eebd68b9d7340d4
DevAppliWeb <dev.appli.web@gmail.com>
2023-12-21 15:24:40 +0100

    Modif param commiter

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -4,6 +4,7 @@ public class BasicExample {
 
 	public static void main(String[] args) {
 		int i;
+		//Ajout de commentaire : Modif author et Mail
 		for(i=0; i<3; i++) {
 			System.out.println("Hello bugs !");
 		}
