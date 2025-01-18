72d510aebaacb2b1c77047079906216299d23a06
dev.appli <dev.appli@gmail.com>
2025-01-18 19:21:08 +0100

    test config Eclipse GitHub

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -4,7 +4,7 @@ public class BasicExample {
 
 	public static void main(String[] args) {
 		int i;
-		//Ajout de commentaire : Modif author et Mail
+		//Résolution du bug
 		for(i=0; i<3; i++) {
 			System.out.println("Hello bugs !");
 		}
